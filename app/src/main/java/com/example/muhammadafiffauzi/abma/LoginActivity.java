dfd893ea955056f3f1358ab49908e8bdddf30af0
Afif <mz.afif28@gmail.com>
2019-04-08 00:15:20 +0700

    adding login adn regiter activities part two

--- app/src/main/java/com/example/muhammadafiffauzi/abma/LoginActivity.java
+++ app/src/main/java/com/example/muhammadafiffauzi/abma/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.muhammadafiffauzi.abma;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -10,22 +13,43 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
 
+    EditText inputName;
+    EditText inputPassword;
+    Button loginSubmit;
+    Button createAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        createAccount = (Button) findViewById(R.id.btn_create_account);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        if(currentUser == null){
-            sendUserToLoginActivity();
+        if(currentUser != null){
+            sendUserToMainActivity();
         }
     }
 
-    private void sendUserToLoginActivity() {
+    private void sendUserToMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent);
     }
