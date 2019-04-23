bd1a4a68b3000c8dc5efab2237ddb92ebb26c03c
Afif <mz.afif28@gmail.com>
2019-04-23 14:07:18 +0700

    add level list activity and add scroll view on activity account

---
+++ app/src/main/java/com/example/muhammadafiffauzi/abma/LevelListActivity.java
@@ -0,0 +1,13 @@
+package com.example.muhammadafiffauzi.abma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_list);
+    }
+}
