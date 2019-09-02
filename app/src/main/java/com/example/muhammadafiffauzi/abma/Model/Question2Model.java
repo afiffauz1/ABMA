3e490b0352edaf417d507be5656c5b62ef62df94
Afif <mz.afif28@gmail.com>
2019-09-03 04:15:04 +0700

    menambah alfabet score view untuk quest 2 dan quest 3

--- app/src/main/java/com/example/muhammadafiffauzi/abma/Model/Question2Model.java
+++ app/src/main/java/com/example/muhammadafiffauzi/abma/Model/Question2Model.java
@@ -3,14 +3,18 @@ package com.example.muhammadafiffauzi.abma.Model;
 public class Question2Model {
     private String idQuest2;
     private String quest2Score;
+    private String date;
+    private String scoreAlfabet;
 
     public Question2Model(){
 
     }
 
-    public Question2Model(String idQuest2, String quest2Score) {
+    public Question2Model(String idQuest2, String quest2Score, String date, String scoreAlfabet) {
         this.idQuest2 = idQuest2;
         this.quest2Score = quest2Score;
+        this.date = date;
+        this.scoreAlfabet = scoreAlfabet;
     }
 
     public String getIdQuest2() {
@@ -27,5 +31,21 @@ public class Question2Model {
 
     public void setQuest2Score(String quest2Score) {
         this.quest2Score = quest2Score;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getScoreAlfabet() {
+        return scoreAlfabet;
+    }
+
+    public void setScoreAlfabet(String scoreAlfabet) {
+        this.scoreAlfabet = scoreAlfabet;
     }
 }
--- app/src/main/java/com/example/muhammadafiffauzi/abma/Model/Question3Model.java
+++ app/src/main/java/com/example/muhammadafiffauzi/abma/Model/Question3Model.java
@@ -3,10 +3,18 @@ package com.example.muhammadafiffauzi.abma.Model;
 public class Question3Model {
     private String idQuest3;
     private String quest3Score;
+    private String date;
+    private String scoreAlfabet;
 
-    public Question3Model(String idQuest3, String quest3Score) {
+    public Question3Model(){
+
+    }
+
+    public Question3Model(String idQuest3, String quest3Score, String date, String scoreAlfabet) {
         this.idQuest3 = idQuest3;
         this.quest3Score = quest3Score;
+        this.date = date;
+        this.scoreAlfabet = scoreAlfabet;
     }
 
     public String getIdQuest3() {
@@ -23,5 +31,21 @@ public class Question3Model {
 
     public void setQuest3Score(String quest3Score) {
         this.quest3Score = quest3Score;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getScoreAlfabet() {
+        return scoreAlfabet;
+    }
+
+    public void setScoreAlfabet(String scoreAlfabet) {
+        this.scoreAlfabet = scoreAlfabet;
     }
 }
