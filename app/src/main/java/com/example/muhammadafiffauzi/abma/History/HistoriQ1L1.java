f54d807f904191a919ee42cc56b39036f60e0600
Afif <mz.afif28@gmail.com>
2019-09-04 03:46:24 +0700

    success to add history score funtion in to question 1 lesson 1

--- app/src/main/java/com/example/muhammadafiffauzi/abma/History/HistoriQ1L1.java
+++ app/src/main/java/com/example/muhammadafiffauzi/abma/History/HistoriQ1L1.java
@@ -3,69 +3,81 @@ package com.example.muhammadafiffauzi.abma.History;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
-import com.example.muhammadafiffauzi.abma.Adapter.Q1L1adapter;
+import com.example.muhammadafiffauzi.abma.Holder.Question1ViewHolder;
 import com.example.muhammadafiffauzi.abma.Model.Question1Model;
 import com.example.muhammadafiffauzi.abma.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class HistoriQ1L1 extends AppCompatActivity {
 
-    private ListView listViewQ1L1;
-    private String currentUser;
+
+    private RecyclerView mScoreList;
     private DatabaseReference mDatabase;
+    private String currentUser;
     private FirebaseAuth mAuth;
-    List<Question1Model> question1Models;
+
+    private ArrayList<Question1Model> arrayList;
+
+    private FirebaseRecyclerOptions<Question1Model> options;
+    private FirebaseRecyclerAdapter<Question1Model, Question1ViewHolder> adapter;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_histori_q1_l1);
 
-
-        listViewQ1L1 = (ListView) findViewById(R.id.listviewQ1L1);
-
-        question1Models = new ArrayList<>();
+        mScoreList = (RecyclerView) findViewById(R.id.myRV);
+        mScoreList.setHasFixedSize(true);
+        mScoreList.setLayoutManager(new LinearLayoutManager(this));
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser().getUid();
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Question1").child(currentUser);
+        mDatabase.keepSynced(true);
 
+        arrayList = new ArrayList<Question1Model>();
+
+        options = new FirebaseRecyclerOptions.Builder<Question1Model>().setQuery(mDatabase, Question1Model.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<Question1Model, Question1ViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull Question1ViewHolder holder, int position, @NonNull Question1Model model) {
+                holder.scoreView.setText(model.getQuest1Score());
+                holder.dateView.setText(model.getDate());
+            }
+
+            @NonNull
+            @Override
+            public Question1ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                return new Question1ViewHolder(LayoutInflater.from(HistoriQ1L1.this).inflate(R.layout.histori_list_layout, viewGroup, false));
+            }
+        };
+
+        mScoreList.setAdapter(adapter);
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                question1Models.clear();
-
-                for(DataSnapshot q1l1snapshot : dataSnapshot.getChildren()){
-                    Question1Model question1Model = q1l1snapshot.getValue(Question1Model.class);
-
-                    question1Models.add(question1Model);
-                }
-
-                Q1L1adapter adapter = new Q1L1adapter(HistoriQ1L1.this, question1Models);
-                listViewQ1L1.setAdapter(adapter);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
 }
---
+++ app/src/main/java/com/example/muhammadafiffauzi/abma/Holder/Question1ViewHolder.java
@@ -0,0 +1,21 @@
+package com.example.muhammadafiffauzi.abma.Holder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.muhammadafiffauzi.abma.Model.Question1Model;
+import com.example.muhammadafiffauzi.abma.R;
+
+public class Question1ViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView scoreView, dateView;
+
+    public Question1ViewHolder(@NonNull View itemView){
+        super(itemView);
+
+        scoreView = itemView.findViewById(R.id.hisScoreView);
+        dateView = itemView.findViewById(R.id.hisDateView);
+    }
+}
